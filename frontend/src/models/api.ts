0cfc3eae8745efe0b853a5846e4be85990703e8e
Felipe Sere <github@felipesere.com>
2024-02-22 20:26:44 +0000

    Replace ky with simple fetch

--- frontend/src/models/api.ts
+++ frontend/src/models/api.ts
@@ -1,18 +1,30 @@
 import {useMutation} from "@tanstack/react-query";
-import ky from "ky"
+import {z} from "zod";
+
+type LoginParms =
+    { email: string, password: string }
+
+const LoginResponseSchema = z.object({
+    token: z.string(),
+    pid: z.string(),
+    name: z.string(),
+    is_verified: z.boolean(),
+})
+
 
 export function useLogin() {
     return useMutation({
-        mutationFn: (params: { email: string, password: string }) => {
-            return ky.post("http://localhost:3000/api/auth/login", {
-                json: {
-                    email: params.email,
-                    password: params.password
+        mutationFn: async (params: LoginParms) => {
+            let response = await fetch("http://localhost:3000/api/auth/login", {
+                body: JSON.stringify(params),
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
                 }
-            }).then((x) => {
-                console.log(x);
-                return x
             })
-        }
+            let body = await response.json();
+            return LoginResponseSchema.parse(body)
+        },
     })
 }
