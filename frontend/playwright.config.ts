5e799e4aeccb7f24a2d287e8840fc7fd9e1a8e17
Felipe Sere <github@felipesere.com>
2024-03-14 21:29:49 +0000

    Connecting to the webserver on localhost

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL: 'http://127.0.0.1:5173',
+    baseURL: 'http://localhost:5173',
     trace: 'on-first-retry',
   },
 
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    url: 'http://127.0.0.1:5173',
+    url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
   }
 });
