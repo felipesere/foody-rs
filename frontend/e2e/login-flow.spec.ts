03a6332ca026b7cbbaee266f2405efb47e846dd5
Felipe Sere <github@felipesere.com>
2024-03-14 08:24:02 +0000

    Use playwright to test the login flow

---
+++ frontend/e2e/login-flow.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test('can do login and redirect back to starting page', async ({ page }) => {
+    // No clue how to correctly parametrise this...
+    await page.goto('http://localhost:3000/');
+
+    await page.getByText("Recipes").click();
+
+    await page
+        .getByLabel('Username')
+        .fill('jim@example.com');
+
+    await page
+        .getByLabel('Password')
+        .fill('rubberduck');
+
+    await page.getByRole('button', { name: /Sign In/ }).click();
+
+    await expect(page).toHaveURL(/.+\/recipes/)
+    await expect(page.locator('nav')).toContainText(/Jim/i);
+});
