4b3fab1189b8d319fe0da10545de1f879eb6019c
Felipe Sere <github@felipesere.com>
2024-03-15 06:41:28 +0000

    Mock out the login API calls

--- frontend/e2e/login-flow.spec.ts
+++ frontend/e2e/login-flow.spec.ts
@@ -4,6 +4,25 @@ test('can do login and redirect back to starting page', async ({ page }) => {
     // No clue how to correctly parametrise this...
     await page.goto('/');
 
+    await page.route('*/**/api/auth/login', async route => {
+        const json = {
+            token: "aaa-bbb-ccc",
+            pid: "11111111",
+            name: "Jim",
+            is_verified: false,
+        }
+        await route.fulfill({ json });
+    });
+
+    await page.route('*/**/api/user/current', async route => {
+        const json = {
+            email: "jim@example.com",
+            name: "Jim",
+            pid: "11111111",
+        }
+        await route.fulfill({ json });
+    });
+
     await page.getByText("Recipes").click();
 
     await page
