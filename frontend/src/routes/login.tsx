0fe47d587bf9dc799a31a554c3aaabb30f4ebdb1
Felipe Sere <github@felipesere.com>
2024-03-21 08:39:15 +0000

    Bump tanstack form

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -69,75 +69,73 @@ function Login() {
   return (
     <div className="content-grid">
       <h3>Login</h3>
-      <form.Provider>
-        <form
-          className={"space-y-4"}
-          onSubmit={(e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            void form.handleSubmit();
+      <form
+        className={"space-y-4"}
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          void form.handleSubmit();
+        }}
+      >
+        <form.Field
+          name="email"
+          validators={{
+            onBlur: z.string().email(),
           }}
-        >
-          <form.Field
-            name="email"
-            validators={{
-              onBlur: z.string().email(),
-            }}
-            children={(emailField) => (
-              <div>
-                <label className={"block"} htmlFor={emailField.name}>
-                  Username
-                </label>
-                <input
-                  type={"text"}
-                  className={"p-2 outline-0 border-black border-2 border-solid"}
-                  autoComplete={"username"}
-                  name={emailField.name}
-                  id={emailField.name}
-                  value={emailField.state.value}
-                  onBlur={emailField.handleBlur}
-                  onChange={(e) => emailField.handleChange(e.target.value)}
-                />
-                {emailField.state.meta.errorMap.onBlur ? (
-                  <em>{emailField.state.meta.errorMap.onBlur}</em>
-                ) : null}
-              </div>
-            )}
-          />
-          <form.Field
-            name="password"
-            children={(passwordField) => (
-              <div>
-                <label className={"block"} htmlFor={passwordField.name}>
-                  Password
-                </label>
-                <input
-                  type={"password"}
-                  className={"p-2 outline-0 border-black border-2 border-solid"}
-                  autoComplete={"current-password"}
-                  name={passwordField.name}
-                  id={passwordField.name}
-                  value={passwordField.state.value}
-                  onChange={(e) => passwordField.handleChange(e.target.value)}
-                />
-              </div>
-            )}
-          />
-          <form.Subscribe
-            selector={(state) => [state.canSubmit]}
-            children={([canSubmit]) => (
-              <button
-                className={"px-2"}
-                type={"submit"}
-                id={"submit"}
-                disabled={!canSubmit}
-              >
-                Sign In
-              </button>
-            )}
-          />
-        </form>
-      </form.Provider>
+          children={(emailField) => (
+            <div>
+              <label className={"block"} htmlFor={emailField.name}>
+                Username
+              </label>
+              <input
+                type={"text"}
+                className={"p-2 outline-0 border-black border-2 border-solid"}
+                autoComplete={"username"}
+                name={emailField.name}
+                id={emailField.name}
+                value={emailField.state.value}
+                onBlur={emailField.handleBlur}
+                onChange={(e) => emailField.handleChange(e.target.value)}
+              />
+              {emailField.state.meta.errorMap.onBlur ? (
+                <em>{emailField.state.meta.errorMap.onBlur}</em>
+              ) : null}
+            </div>
+          )}
+        />
+        <form.Field
+          name="password"
+          children={(passwordField) => (
+            <div>
+              <label className={"block"} htmlFor={passwordField.name}>
+                Password
+              </label>
+              <input
+                type={"password"}
+                className={"p-2 outline-0 border-black border-2 border-solid"}
+                autoComplete={"current-password"}
+                name={passwordField.name}
+                id={passwordField.name}
+                value={passwordField.state.value}
+                onChange={(e) => passwordField.handleChange(e.target.value)}
+              />
+            </div>
+          )}
+        />
+        <form.Subscribe
+          selector={(state) => [state.canSubmit]}
+          children={([canSubmit]) => (
+            <button
+              className={"px-2"}
+              type={"submit"}
+              id={"submit"}
+              disabled={!canSubmit}
+            >
+              Sign In
+            </button>
+          )}
+        />
+      </form>
     </div>
   );
 }
