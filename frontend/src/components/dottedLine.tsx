4108b733cefc9299f27c904f6afe72fbf158a690
Felipe Sere <github@felipesere.com>
2024-04-21 20:52:54 +0100

    Make lines and dots match colors

--- frontend/src/components/dottedLine.tsx
+++ frontend/src/components/dottedLine.tsx
@@ -1,5 +1,5 @@
 export function DottedLine() {
   return (
-    <div className={"flex-auto bg-white mx-0.5 bg-repeat-x dotted-line"} />
+    <div className={"flex-auto mx-0.5 bg-repeat-x dotted-line"} />
   );
 }
