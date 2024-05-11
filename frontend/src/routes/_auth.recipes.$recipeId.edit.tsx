870bf58ed7727fd2497bc1ab000e1a2070ead641
Felipe Sere <github@felipesere.com>
2024-05-11 16:40:16 +0100

    Lift the actual form into its own component

--- frontend/src/routes/_auth.recipes.$recipeId.edit.tsx
+++ frontend/src/routes/_auth.recipes.$recipeId.edit.tsx
@@ -8,7 +8,7 @@ import {
   useRole,
 } from "@floating-ui/react";
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
-import { type Quantity, useRecipeOptions } from "../apis/recipes.ts";
+import {type Quantity, type Recipe, useRecipeOptions} from "../apis/recipes.ts";
 import type { Ingredient } from "../apis/ingredients.ts";
 
 import classnames from "classnames";
@@ -33,11 +33,6 @@ export const Route = createFileRoute("/_auth/recipes/$recipeId/edit")({
 function EditRecipePage() {
   const { recipeId } = Route.useParams();
   const { token } = Route.useRouteContext();
-  const { data: recipe } = useSuspenseQuery(
-    useRecipeOptions(token, Number(recipeId)),
-  );
-
-  // const data = useRecipe(token, Number(recipeId));
   const navigate = useNavigate({ from: "/recipes/$recipeId/edit" });
 
   const { refs, context } = useFloating({
@@ -53,58 +48,6 @@ function EditRecipePage() {
 
   // Merge all the interactions into prop getters
   const { getFloatingProps } = useInteractions([click, dismiss, role]);
-  const [sourceKind, setSourceKind] = useState<"book" | "website">("book");
-
-  const [additionalIngredients, setAdditionalIngredients] = useState<
-    Array<{ ingredient: Ingredient; quantity: Quantity }>
-  >([]);
-
-  const maybeBook =
-    recipe.source === "book"
-      ? { page: recipe.page, title: recipe.title }
-      : null;
-  const maybeWebsite = recipe.source === "website" ? { url: recipe.url } : null;
-
-  type Website = {
-    url: string;
-  };
-  function EditWebsite(props: { website: Website | null }) {
-    return (
-      <fieldset className={"border-black border-2 p-2"}>
-        <legend className={"px-2"}>Website</legend>
-        <div className={"flex flex-row gap-2"}>
-          <label>URL</label>
-          <FancyInput3 value={props.website?.url || ""} />
-        </div>
-      </fieldset>
-    );
-  }
-
-  type Book = {
-    page: number;
-    title: string;
-  };
-
-  function EditBook(props: { book: Book | null }) {
-    return (
-      <fieldset className={"border-black border-2 p-2"}>
-        <legend className={"px-2"}>Book</legend>
-
-        <div className={"flex flex-row gap-2"}>
-          <label>Title</label>
-          <FancyInput3 name={"bookTitle"} value={props.book?.title || ""} />
-        </div>
-
-        <div className={"flex flex-row gap-2"}>
-          <label>Page</label>
-          <FancyInput3
-            name={"bookPage"}
-            value={props.book?.page.toString() || ""}
-          />
-        </div>
-      </fieldset>
-    );
-  }
 
   return (
     <FloatingOverlay
@@ -122,157 +65,153 @@ function EditRecipePage() {
               "m-2 p-4 bg-white w-full h-full max-w-2xl relative border-solid border-black border-2 space-y-4",
           })}
         >
-          <p className={"font-bold"}>Edit recipe</p>
-          <form
-            id="editRecipe"
-            onSubmit={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
-
-              // const _newRecipeForm = new FormData(e.currentTarget);
-            }}
-          >
-            <fieldset
-              className={"border-black border-2 p-2 flex flex-row gap-4"}
-            >
-              <legend className={"px-2"}>Name</legend>
-              <FancyInput3 name={"name"} value={recipe.name} />
-            </fieldset>
-
-            <fieldset
-              className={"border-black border-2 p-2 flex flex-row gap-4"}
-            >
-              <legend className={"px-2"}>Kind of source</legend>
-              <div className="flex flex-row gap-1">
-                <input
-                  type="radio"
-                  id="book"
-                  name="source"
-                  value="book"
-                  checked={sourceKind === "book"}
-                  onChange={() => setSourceKind("book")}
-                />
-                <label htmlFor="book">Book</label>
-              </div>
-
-              <div className="flex flex-row gap-1">
-                <input
-                  type="radio"
-                  id="website"
-                  name="source"
-                  value="website"
-                  checked={sourceKind === "website"}
-                  onChange={() => setSourceKind("website")}
-                />
-                <label htmlFor="website">Website</label>
-              </div>
-            </fieldset>
-
-            {sourceKind === "book" ? (
-              <EditBook book={maybeBook} />
-            ) : (
-              <EditWebsite website={maybeWebsite} />
-            )}
-
-            <fieldset className={"border-black border-2 p-2"}>
-              <legend className={"px-2"}>Ingredients</legend>
-              <ol>
-                {recipe.ingredients.map((ingredient) => {
-                  return (
-                    <li
-                      key={ingredient.id}
-                      className={"flex flex-row justify-between"}
-                    >
-                      <p>{ingredient.name}</p>
-                      <DottedLine className={"flex-shrink"} />
-                      <FancyInput3
-                        name={`ingredient[${ingredient.name}]`}
-                        value={humanize(ingredient.quantity[0])}
-                      />
-                    </li>
-                  );
-                })}
-                {additionalIngredients.map(({ ingredient, quantity }) => {
-                  return (
-                    <li
-                      key={ingredient.id}
-                      className={"flex flex-row justify-between"}
-                    >
-                      <p>{ingredient.name}</p>
-                      <DottedLine className={"flex-shrink"} />
-                      <FancyInput3
-                        name={`quantity[${ingredient.name}]`}
-                        value={humanize(quantity)}
-                      />
-                    </li>
-                  );
-                })}
-              </ol>
-
-              <FindIngredient
-                className={"mt-2"}
-                token={token}
-                onIngredient={(ingredient, quantity) => {
-                  setAdditionalIngredients((before) => [
-                    ...before,
-                    { ingredient, quantity },
-                  ]);
-                  // here is where we can add an ingredient and quantity to a recipe!
-                }}
-              />
-            </fieldset>
-          </form>
-          <Divider />
-          <ButtonGroup>
-            <button
-              type="button"
-              className={"px-2"}
-              onClick={() => navigate({ to: "/recipes" })}
-            >
-              Close
-            </button>
-
-            <button type="submit" form={"editRecipe"} className={"px-2"}>
-              Save
-            </button>
-
-            <button
-              type="button"
-              className={"px-2"}
-              onClick={() => navigate({ to: "/recipes" })}
-            >
-              Cancel
-            </button>
-          </ButtonGroup>
+          <EditRecipeFrom token={token} recipeId={Number(recipeId)} />
         </div>
       </FloatingFocusManager>
     </FloatingOverlay>
   );
 }
 
-// function FancyInput(props: { value: string }) {
-//   return (
-//     <input
-//       className={"flex-shrink w-fit"}
-//       type={"text"}
-//       defaultValue={props.value}
-//     />
-//   );
-// }
+function EditRecipeFrom(props: {token: string, recipeId: Recipe["id"]}) {
+  const token = props.token
+  const { data: recipe } = useSuspenseQuery(
+      useRecipeOptions(token, props.recipeId),
+  );
+  const navigate = useNavigate({ from: "/recipes/$recipeId/edit" });
+  const [sourceKind, setSourceKind] = useState<"book" | "website">("book");
 
-/*
-function FancyInput2(props: { value: string }) {
-  const len = props.value.length;
+  const [additionalIngredients, setAdditionalIngredients] = useState<
+      Array<{ ingredient: Ingredient; quantity: Quantity }>
+  >([]);
+
+  const maybeBook =
+      recipe.source === "book"
+          ? { page: recipe.page, title: recipe.title }
+          : null;
+  const maybeWebsite = recipe.source === "website" ? { url: recipe.url } : null;
   return (
-    <input
-      className={"flex-shrink"}
-      style={{ width: `${len}ch` }}
-      type={"text"}
-      defaultValue={props.value}
-    />
+    <>
+      <p className={"font-bold"}>Edit recipe</p>
+      <form
+        id="editRecipe"
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+
+          // const _newRecipeForm = new FormData(e.currentTarget);
+        }}
+      >
+        <fieldset className={"border-black border-2 p-2 flex flex-row gap-4"}>
+          <legend className={"px-2"}>Name</legend>
+          <FancyInput3 name={"name"} value={recipe.name} />
+        </fieldset>
+
+        <fieldset className={"border-black border-2 p-2 flex flex-row gap-4"}>
+          <legend className={"px-2"}>Kind of source</legend>
+          <div className="flex flex-row gap-1">
+            <input
+              type="radio"
+              id="book"
+              name="source"
+              value="book"
+              checked={sourceKind === "book"}
+              onChange={() => setSourceKind("book")}
+            />
+            <label htmlFor="book">Book</label>
+          </div>
+
+          <div className="flex flex-row gap-1">
+            <input
+              type="radio"
+              id="website"
+              name="source"
+              value="website"
+              checked={sourceKind === "website"}
+              onChange={() => setSourceKind("website")}
+            />
+            <label htmlFor="website">Website</label>
+          </div>
+        </fieldset>
+
+        {sourceKind === "book" ? (
+          <EditBook book={maybeBook} />
+        ) : (
+          <EditWebsite website={maybeWebsite} />
+        )}
+
+        <fieldset className={"border-black border-2 p-2"}>
+          <legend className={"px-2"}>Ingredients</legend>
+          <ol>
+            {recipe.ingredients.map((ingredient) => {
+              return (
+                <li
+                  key={ingredient.id}
+                  className={"flex flex-row justify-between"}
+                >
+                  <p>{ingredient.name}</p>
+                  <DottedLine className={"flex-shrink"} />
+                  <FancyInput3
+                    name={`ingredient[${ingredient.name}]`}
+                    value={humanize(ingredient.quantity[0])}
+                  />
+                </li>
+              );
+            })}
+            {additionalIngredients.map(({ ingredient, quantity }) => {
+              return (
+                <li
+                  key={ingredient.id}
+                  className={"flex flex-row justify-between"}
+                >
+                  <p>{ingredient.name}</p>
+                  <DottedLine className={"flex-shrink"} />
+                  <FancyInput3
+                    name={`quantity[${ingredient.name}]`}
+                    value={humanize(quantity)}
+                  />
+                </li>
+              );
+            })}
+          </ol>
+
+          <FindIngredient
+            className={"mt-2"}
+            token={token}
+            onIngredient={(ingredient, quantity) => {
+              setAdditionalIngredients((before) => [
+                ...before,
+                { ingredient, quantity },
+              ]);
+              // here is where we can add an ingredient and quantity to a recipe!
+            }}
+          />
+        </fieldset>
+      </form>
+      <Divider />
+      <ButtonGroup>
+        <button
+          type="button"
+          className={"px-2"}
+          onClick={() => navigate({ to: "/recipes" })}
+        >
+          Close
+        </button>
+
+        <button type="submit" form={"editRecipe"} className={"px-2"}>
+          Save
+        </button>
+
+        <button
+          type="button"
+          className={"px-2"}
+          onClick={() => navigate({ to: "/recipes" })}
+        >
+          Cancel
+        </button>
+      </ButtonGroup>
+    </>
   );
 }
- */
 
 interface InputAutosizeProps extends InputHTMLAttributes<HTMLInputElement> {
   value: string;
@@ -300,5 +239,46 @@ export default function FancyInput3({
         onChange={(e) => setX(e.target.value)}
       />
     </div>
+  );
+}
+
+type Website = {
+  url: string;
+};
+function EditWebsite(props: { website: Website | null }) {
+  return (
+      <fieldset className={"border-black border-2 p-2"}>
+        <legend className={"px-2"}>Website</legend>
+        <div className={"flex flex-row gap-2"}>
+          <label>URL</label>
+          <FancyInput3 value={props.website?.url || ""} />
+        </div>
+      </fieldset>
+  );
+}
+
+type Book = {
+  page: number;
+  title: string;
+};
+
+function EditBook(props: { book: Book | null }) {
+  return (
+      <fieldset className={"border-black border-2 p-2"}>
+        <legend className={"px-2"}>Book</legend>
+
+        <div className={"flex flex-row gap-2"}>
+          <label>Title</label>
+          <FancyInput3 name={"bookTitle"} value={props.book?.title || ""} />
+        </div>
+
+        <div className={"flex flex-row gap-2"}>
+          <label>Page</label>
+          <FancyInput3
+              name={"bookPage"}
+              value={props.book?.page.toString() || ""}
+          />
+        </div>
+      </fieldset>
   );
 }
