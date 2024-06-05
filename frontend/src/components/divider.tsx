8c5f032cf4649cc081bbad457f2bcb7dd6253fca
Felipe Sere <github@felipesere.com>
2024-06-05 12:20:56 +0200

    Add an optional label to the divier

--- frontend/src/components/divider.tsx
+++ frontend/src/components/divider.tsx
@@ -1,3 +1,16 @@
-export function Divider() {
+type DividerProps = {
+  label?: string;
+};
+
+export function Divider(props: DividerProps) {
+  if (props.label) {
+    return (
+      <div className={"flex flex-row"}>
+        <hr className="h-0.5 my-2 w-6 bg-black border-0 mr-2 translate-y-1" />
+        {props.label}
+        <hr className="h-0.5 my-2 w-full bg-black border-0 ml-2 translate-y-1" />
+      </div>
+    );
+  }
   return <hr className="h-0.5 my-2 w-full bg-black border-0" />;
 }
