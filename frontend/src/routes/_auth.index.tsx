f015167396a3f11a0da12967301a335ef604cca7
Felipe Sere <github@felipesere.com>
2024-10-20 20:28:43 +0100

    Odd changes in tanstack from `touchedErrors`

--- frontend/src/routes/_auth.index.tsx
+++ frontend/src/routes/_auth.index.tsx
@@ -101,8 +101,8 @@ function NewShoppinglist(props: { token: string }) {
 function FieldInfo({ field }: { field: FieldApi<any, any, any, any> }) {
   return (
     <>
-      {field.state.meta.touchedErrors ? (
-        <em>{field.state.meta.touchedErrors}</em>
+      {field.state.meta.isTouched && field.state.meta.errors.length ? (
+        <em>{field.state.meta.errors}</em>
       ) : null}
       {field.state.meta.isValidating ? "Validating..." : null}
     </>
