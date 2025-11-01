efc48dfe41cd563441a28f36ccf69ecd245386d6
Felipe Sere <github@felipesere.com>
2025-11-01 21:57:18 +0000

    Properly center the Divider

--- frontend/src/components/divider.tsx
+++ frontend/src/components/divider.tsx
@@ -6,12 +6,16 @@ type DividerProps = {
 export function Divider(props: DividerProps) {
   if (props.label) {
     return (
-      <div className={"flex flex-row"}>
-        <hr className="h-0.5 my-0.5lh w-6ch bg-black border-0 mr-2ch  -translate-y-0.5lh" />
+      <div className={"flex flex-row items-center h-1lh"}>
+        <hr className="h-[2px] w-6ch bg-black border-0 mr-1ch" />
         <span className={props.className}>{props.label}</span>
-        <hr className="h-0.5 my-0.5lh w-full bg-black border-0 ml-2ch -translate-y-0.5lh" />
+        <hr className="h-[2px] w-full bg-black border-0 ml-1ch" />
       </div>
     );
   }
-  return <hr className="h-0.5 my-1lh w-full bg-black border-0" />;
+  return (
+    <div className="flex items-center h-1lh">
+      <hr className="h-[2px] w-full bg-black border-0" />
+    </div>
+  );
 }
