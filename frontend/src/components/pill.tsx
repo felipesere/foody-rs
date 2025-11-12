5afb641a95120318c776890633ff708ebea286a5
Felipe Sere <github@felipesere.com>
2025-11-12 22:43:42 +0000

    Min height for the pill is 1lh

--- frontend/src/components/pill.tsx
+++ frontend/src/components/pill.tsx
@@ -8,18 +8,18 @@ export function Pill(props: PillProps) {
   return (
     <div
       className={classnames(
-        "flex flex-row gap-x-1ch px-1ch border-solid border-black border-2",
+        "flex flex-row items-center gap-x-1ch px-1ch border-solid border-black border-2 h-fit",
       )}
     >
       {props.onClose && (
         <span
-          className={"hover:bg-gray-300 cursor-pointer"}
+          className={"hover:bg-gray-300 cursor-pointer min-h-1lh"}
           onClick={() => props.onClose?.(props.value)}
         >
           &#x2715;
         </span>
       )}
-      <p>{props.value}</p>
+      <p className="leading-none my-0">{props.value}</p>
     </div>
   );
 }
