12b57e107b83954d6fe978297850e107ce3bfbe3
Felipe Sere <github@felipesere.com>
2026-07-06 23:07:33 +0100

    Remove Rust code

--- src/models/meal_plans.rs
+++
@@ -1,6 +0,0 @@
-use super::_entities::meal_plans::ActiveModel;
-use sea_orm::entity::prelude::*;
-
-impl ActiveModelBehavior for ActiveModel {
-    // extend activemodel below (keep comment for generators)
-}
--- tests/tasks/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod seed;
-
-pub mod gql_schema;
